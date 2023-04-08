af1840847fda9e879726f5d048c8b16a904a8fc8
dsamia25 <42752755+dsamia25@users.noreply.github.com>
2023-04-08 16:17:05 -0700

    Added Turret Stats
    Added a DirectAttack and SplashAttack stat to turrets.

--- FroggyDefense/Assets/Scripts/Core/Turret.cs
+++ FroggyDefense/Assets/Scripts/Core/Turret.cs
@@ -16,47 +16,53 @@ namespace FroggyDefense.Core
     public class Turret : MonoBehaviour, IUseWeapon
     {
         [Space]
-        [Header("Turret Componenets")]
+        [Header("Turret Components")]
         [Space]
         [SerializeField] private GameObject m_TurretHead;               // The moving head of the turret.
         [SerializeField] private GameObject m_TurretBody;               // The stationary base of the turret.
         [SerializeField] private RectTransform m_HighlightRangeCircle;  // The target radius highlight circle when the turret is moused over.
         [SerializeField] private RectTransform m_SelectRangeCircle;     // The target radius highlight circle when the turret is selected.
+        [SerializeField] protected Weapon m_Weapon = null;
 
         [Space]
         [Header("Target Settings")]
         [Space]
         [SerializeField] private TargetSetting m_TargetSetting = TargetSetting.ClosestTarget;   // Determines how the turret should prioritize focusing enemies it sees.
         [SerializeField] private LayerMask m_TargetLayer = 0;                                   // Which layer in which the turret looks for targets.
-        [SerializeField] private float m_TargetRadius = 1;                                      // How far the turret can see.
+        [SerializeField] private float _targetRadius = 1;                                      // How far the turret can see.
         [SerializeField] private float m_TargetCheckFrequency = .1f;                            // How often the turret checks for new targets.
         [SerializeField] private float m_ChangeFocusTime = 2f;                                  // How long the turret will focus on an enemy before checking for another focus.
         [SerializeField] private GameObject m_Focus = null;                                     // The enemy this turret will attack.
 
-        [Space]
-        [Header("Attack Settings")]
-        [Space]
-        [SerializeField] protected Weapon m_Weapon = null;
-        [SerializeField] private float m_AttackRadius = 1;                                      // How far the turret can attack it's focus.
-        [SerializeField] private float m_AttackCooldown = 1;                                    // How many attacks until the 
+        public float m_TargetRadius { get => _targetRadius; set { _targetRadius = value; } }
 
-        private float _attackCooldown = 0f;
+        [Space]
+        [Header("Turret Stats")]
+        [Space]
+        [SerializeField] private float _directDamage = 1f;
+        [SerializeField] private float _splashDamage = 1f;
+        [SerializeField] private float _attackRadius = 1f;                                      // How far the turret can attack it's focus.
+        [SerializeField] private float _attackCooldown = 1f;
+
+        public float m_DirectDamage { get => _directDamage; set { _directDamage = value; } }
+        public float m_SplashDamage { get => _splashDamage; set { _splashDamage = value; } }
+        public float m_AttackRadius { get => _attackRadius; set { _attackRadius = value; } }
+        public float m_AttackCooldown { get => _attackCooldown; set { _attackCooldown = value; } }
+
+        private float _currAttackCooldown = 0f;
         private float _targetCheckCooldown = 0f;
         private float _changeFocusCooldown = 0f;
 
         private void Start()
         {
-            if (m_AttackRadius < m_TargetRadius)
+            if (m_AttackRadius < _targetRadius)
             {
-                m_AttackRadius = m_TargetRadius + 1;
+                m_AttackRadius = _targetRadius + 1;
             }
 
-            // Set target circle radii.
-            var circleRadius = 2 * m_TargetRadius;
-            m_HighlightRangeCircle.sizeDelta = new Vector2(circleRadius, circleRadius);
-            m_SelectRangeCircle.sizeDelta = new Vector2(circleRadius, circleRadius);
+            UpdateTargetRadiusOverlay();
 
-            _attackCooldown = 0f;
+            _currAttackCooldown = 0f;
             _targetCheckCooldown = m_TargetCheckFrequency;
             _changeFocusCooldown = 0f;
         }
@@ -78,7 +84,7 @@ namespace FroggyDefense.Core
                 // Rotate Turret.
                 m_TurretHead.transform.rotation = Quaternion.Euler(0f, 0f, SupportMethods.AngleBetweenTwoPoints(m_Focus.transform.position, m_TurretHead.transform.position) - 90);
 
-                if (_attackCooldown <= 0)
+                if (_currAttackCooldown <= 0)
                 {
                     Attack();
                 }
@@ -90,9 +96,18 @@ namespace FroggyDefense.Core
                 }
             }
 
-            _attackCooldown -= Time.deltaTime;
+            _currAttackCooldown -= Time.deltaTime;
             _targetCheckCooldown -= Time.deltaTime;
             _changeFocusCooldown -= Time.deltaTime;
+        }
+
+        private void UpdateTargetRadiusOverlay()
+        {
+            // Set target circle radii.
+            var circleRadius = 2 * _targetRadius;
+            m_HighlightRangeCircle.sizeDelta = new Vector2(circleRadius, circleRadius);
+            m_SelectRangeCircle.sizeDelta = new Vector2(circleRadius, circleRadius);
+
         }
 
         /// <summary>
@@ -101,12 +116,13 @@ namespace FroggyDefense.Core
         public void Attack()
         {
             m_Weapon.Shoot((m_Focus.transform.position - transform.position).normalized);
-            _attackCooldown = m_AttackCooldown;
+            _currAttackCooldown = m_AttackCooldown;
         }
 
         public void OnMouseEnter()
         {
             Debug.Log("Mouse over turret.");
+            UpdateTargetRadiusOverlay();
             m_HighlightRangeCircle.gameObject.SetActive(true);
         }
         
@@ -166,7 +182,7 @@ namespace FroggyDefense.Core
         /// <returns></returns>
         public Collider2D[] GetTargets()
         {
-            return Physics2D.OverlapCircleAll(transform.position, m_TargetRadius, (m_TargetLayer == 0 ? gameObject.layer : m_TargetLayer));
+            return Physics2D.OverlapCircleAll(transform.position, _targetRadius, (m_TargetLayer == 0 ? gameObject.layer : m_TargetLayer));
         }
 
         /// <summary>
@@ -175,7 +191,7 @@ namespace FroggyDefense.Core
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(transform.position, m_TargetRadius);
+            Gizmos.DrawWireSphere(transform.position, _targetRadius);
 
             Gizmos.DrawWireSphere(transform.position, m_AttackRadius);
         }
@@ -183,13 +199,13 @@ namespace FroggyDefense.Core
         // TODO: Make a way to calculate the turret's damage.
         public float GetDirectDamage()
         {
-            return 1f;
+            return m_DirectDamage;
         }
 
         // TODO: Make a way to calculate the turret's damage.
         public float GetSplashDamage()
         {
-            return 1f;
+            return m_SplashDamage;
         }
     }
 }
