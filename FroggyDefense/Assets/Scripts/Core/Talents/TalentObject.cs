8f62ea2a8e882721b61527a9fb33eb3b800dc50e
dsamia25 <42752755+dsamia25@users.noreply.github.com>
2023-10-07 16:57:06 -0700

    Creating base for talent system
    - Creating scripts:
         - TalentNode: Each node on the talent web. Checks talent requirements and links to other nodes.
         - TalentEffect (Rename to Talent?): The actual talent effect.
         - TalentEffectObject (Rename to TalentObject?): ScriptableObject builder for TalentEffect similar to an ActionObject or a SpellObject.
         - UI scripts for managing the UI.

---
+++ FroggyDefense/Assets/Scripts/Core/Talents/TalentObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FroggyDefense.Core.Talents
+{
+    public class TalentObject : ScriptableObject
+    {
+        
+    }
+}
